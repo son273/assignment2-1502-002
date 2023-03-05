7c92856426f72550245646fa2323a40aac0304de
son273 <jps.son273@mtroyal.cagit statusgit config --global user.email jps.son273@mtroyal.ca>
2023-03-05 13:20:54 -0700

    started doing works

--- src/mru/store/controller/AppDriver.java
+++ src/mru/store/controller/AppDriver.java
@@ -1,6 +1,63 @@
 package mru.store.controller;
 
-public class AppDriver {
-	//Manages and runs the application
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+import mru.store.model.Toys;
+import mru.store.view.AppMenu;
+//Manages and runs the application
+
+public class AppDriver {
+	private ArrayList<Toys> toy;
+	private AppMenu menu;	
+	private final String FILE_PATH = "res/toys.txt";
+	
+	// 
+	
+	public AppDriver() {
+		toy = new ArrayList<>();
+		menu = new AppMenu();
+		menuOptions();
+		//
+	}
+	
+	public void menuOptions() {
+		//menu.mainmenu
+	}
+	
+	
+	
+	//add save and exit
+	//add load data up method
+	//
+	
+	private void loadData() {
+		File txt = new File(FILE_PATH); // opens file
+		String currentLine;
+		String[] splittedLine;
+
+		if (txt.exists()) { // checks if file exists
+			//TRY HERE
+			Scanner fileReader = new Scanner(txt); // Creates scanner class to read file
+
+			while (fileReader.hasNextLine()) { // checks if the line exists
+
+				currentLine = fileReader.nextLine(); // reads current line
+				splittedLine = currentLine.split(","); // splits line at ,
+					String name = splittedLine[0]; //
+					int balance = Integer.parseInt(splittedLine[1]);//
+					int wins = Integer.parseInt(splittedLine[2]); //
+			//CATCH HERE
+				}
+			
+			fileReader.close();
+		}
+	}
+	
+	
+	
+	
+	
+	
 }
---
+++ src/mru/store/model/Toys.java
@@ -0,0 +1,14 @@
+package mru.store.model;
+
+public abstract class Toys {
+	// need setters/getters
+	//may need more
+	//may need to overload toString and other methods
+	
+	
+	
+	
+	
+	
+	
+}

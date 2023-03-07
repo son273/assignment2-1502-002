0decb46011a68020bbb624a006447ddf88e0fd5d
caesarliang <caesarliang@localhost>
2023-03-07 14:30:02 -0700

    save

--- src/mru/store/model/Animals.java
+++ src/mru/store/model/Animals.java
@@ -1,5 +1,84 @@
 package mru.store.model;
 
-public class Animals {
+public abstract class Animals extends Toys {
+
+	private int serialNum;
+	private String name;
+	private String brand;
+	private int price;
+	private int stockCount;
+	private int minAge;
+	private String material;
+	private String size;
+
+	public int getSerialNum() {
+		return serialNum;
+	}
+
+	public void setSerialNum(int serialNum) {
+		this.serialNum = serialNum;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getStockCount() {
+		return stockCount;
+	}
+
+	public void setStockCount(int stockCount) {
+		this.stockCount = stockCount;
+	}
+
+	public int getMinAge() {
+		return minAge;
+	}
+
+	public void setMinAge(int minAge) {
+		this.minAge = minAge;
+	}
+
+	public String getMaterial() {
+		return material;
+	}
+
+	public void setMaterial(String material) {
+		this.material = material;
+	}
+
+	public String getSize() {
+		return size;
+	}
+
+	public void setSize(String size) {
+		this.size = size;
+	}
+
+	@Override
+	public String format() {
+		return serialNum + ";" + name + ";" + brand + ";" + price + ";" + stockCount + ";" + minAge + ";" + material
+				+ ";" + size;
+	}
 
 }
--- src/mru/store/model/Figures.java
+++ src/mru/store/model/Figures.java
@@ -1,5 +1,75 @@
 package mru.store.model;
 
-public class Figures {
+public abstract class Figures extends Toys {
+
+	private int serialNum;
+	private String name;
+	private String brand;
+	private int price;
+	private int stockCount;
+	private int minAge;
+	private String classification;
+
+	public int getSerialNum() {
+		return serialNum;
+	}
+
+	public void setSerialNum(int serialNum) {
+		this.serialNum = serialNum;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getStockCount() {
+		return stockCount;
+	}
+
+	public void setStockCount(int stockCount) {
+		this.stockCount = stockCount;
+	}
+
+	public int getMinAge() {
+		return minAge;
+	}
+
+	public void setMinAge(int minAge) {
+		this.minAge = minAge;
+	}
+
+	public String getClassification() {
+		return classification;
+	}
+
+	public void setClassification(String classification) {
+		this.classification = classification;
+	}
+
+	@Override
+	public String format() {
+		return serialNum + ";" + name + ";" + brand + ";" + price + ";" + stockCount + ";" + minAge + ";"
+				+ classification;
+	}
 
 }

d410866a689c35ee1290247d6f0b42bd7fb73111
caesarliang <caesarliang@localhost>
2023-03-08 16:35:38 -0700

    save

--- src/mru/store/view/AppMenu.java
+++ src/mru/store/view/AppMenu.java
@@ -9,6 +9,7 @@ public class AppMenu {
 	public AppMenu() {
 		input = new Scanner(System.in);
 	}
+
 	public void welcomeMessage() {
 		System.out.println("***********************************************");
 		System.out.println("* WELCOME TO TOYSTORE COMPANY *");
@@ -37,11 +38,136 @@ public class AppMenu {
 		return option;
 	}
 
+	public int promptOption() {
+		System.out.println("Enter Option:");
+		int option = input.nextInt();
+		return option;
+	}
+
+	public void promptEnterKey() {
+		System.out.println("Press Enter to Continue...");
+		input.nextLine();
+	}
+
+	public int promptSerialNum() {
+		System.out.println("Enter Serial Number: ");
+		int serialNum = input.nextInt();
+		return serialNum;
+	}
+
+	public String prompToyName() {
+		System.out.println("Enter Toy Name: ");
+		String toyName = input.next();
+		return toyName;
+	}
+
+	public String prompBrandName() {
+		System.out.println("Enter Brand Name: ");
+		String brandName = input.next();
+		return brandName;
+	}
+
+	public float promptToyPrice() {
+		System.out.println("Enter Toy Price: ");
+		float toyPrice = input.nextFloat();
+		return toyPrice;
+	}
+
+	public int promptAvailability() {
+		System.out.println("Enter Available Counts: ");
+		int availability = input.nextInt();
+		return availability;
+	}
+
+	public int promptAge() {
+		System.out.println("Enter Appropriate Age: ");
+		int minAge = input.nextInt();
+		return minAge;
+	}
+
+	public String promptFigureClass() {
+		System.out.println("Enter Figure Classification (Action, Doll, or Historic): ");
+		String figureClass = input.next().toLowerCase();
+		return figureClass;
+	}
+
+	public String promptAnimalMaterial() {
+		System.out.println("Enter Animal Material: ");
+		String animalMaterial = input.next().toLowerCase();
+		return animalMaterial;
+	}
+
+	public String promptAnimalSize() {
+		System.out.println("Enter Animal Size (Small, Medium, or Large): ");
+		String animalSize = input.next().toLowerCase();
+		return animalSize;
+	}
+
+	public String promptPuzzleType() {
+		System.out.println("Enter Puzzle Type (Mechanical, Cryptic, Logic, Trivia, or Riddle): ");
+		String puzzleType = input.next().toLowerCase();
+		return puzzleType;
+	}
+
+	public int promptBoardGameMinPlayers() {
+		System.out.println("Enter Minimum Number of Players: ");
+		int minPlayers = input.nextInt();
+		return minPlayers;
+	}
+
+	public int promptBoardGameMaxPlayers() {
+		System.out.println("Enter Maximum Number of Players: ");
+		int maxPlayers = input.nextInt();
+		return maxPlayers;
+	}
+
+	public String promptBoardGameDesigners() {
+		System.out.println("Enter Designer Names (Seperate Names Using ','): ");
+		String designerName = input.next();
+		return designerName;
+	}
+
+	public void toyAddedMessage() {
+		System.out.println("New Toy Added!");
+	}
+
+	public char promptRemoval() {
+		System.out.println("Do you want to remove it? (Y/N)");
+		char option = input.nextLine().toLowerCase().charAt(0);
+		return option;
+	}
+
+	public void toyRemovedMessage() {
+		System.out.println("Item Removed!");
+	}
+
 	public void saveMessage() {
 		System.out.println("Saving Data Into Database\n");
 		System.out.println("***********************************************");
 		System.out.println("* THANKS FOR VISTING US! *");
 		System.out.println("***********************************************");
+	}
+
+	public void validateNumNotValid() {
+		System.out.println("This is Not an Integer Number! Try Again. ");
+	}
+
+	public void validateOptionNotValid() {
+		System.out.println("This is Not an Valid Option! Try Again. ");
+	}
+
+	public void validateExistingSN() {
+		System.out.println("A Toy With This Serial Number Already Exists! Try Again. ");
+
+	}
+
+	public void validateSN() {
+		System.out.println("The Serial Number should only contain Digits! Try Again. ");
+
+	}
+
+	public void validateSNLength() {
+		System.out.println("The Serial Number's legnth MUST be 10 digits! Try Again. ");
 
 	}
 
